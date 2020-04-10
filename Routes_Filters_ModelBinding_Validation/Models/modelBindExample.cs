ec60c04bb90e7328930fea62019c9c8957478e44
Hwei Hua Tan <hweihuatan@mail.usf.edu>
2020-04-10 12:30:51 -0400

    Add project files.

---
+++ Routes_Filters_ModelBinding_Validation/Models/modelBindExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class Education
+    {
+        public string University { get; set; }
+        public string Degree { get; set; }
+        public string Major { get; set; }
+    }
+}
